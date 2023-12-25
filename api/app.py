7fd2959813de4e34b9e9bf53869426a01f7c57e4
Nguyen Pham Phu Thinh <105217439+Tieu-Thanh@users.noreply.github.com>
2023-12-25 22:47:58 +0700

    user & authen

--- api/app.py
+++ api/app.py
@@ -1,11 +1,15 @@
 from flask import Flask
 from config import app_config
-
+from resources.auth_api import auth_bp
+from resources.user_api import user_bp
 app = Flask(__name__)
 app.config.from_object(app_config)
 
 
-# Add other application configurations or extensions here
+# Blueprint registration
+app.register_blueprint(auth_bp, url_prefix='/api/auth')
+app.register_blueprint(user_bp, url_prefix='/api/user')
+
 
 @app.route('/')
 def hello_world():
---
+++ api/models/User.py
@@ -0,0 +1,15 @@
+# models.py
+class User:
+    def __init__(self, uid, display_name=None, phone_number=None, photo_url=None):
+        self.uid = uid
+        self.display_name = display_name
+        self.phone_number = phone_number
+        self.photo_url = photo_url
+
+    def to_dict(self):
+        return {
+            'uid': self.uid,
+            'display_name': self.display_name,
+            'phone_number': self.phone_number,
+            'photo_url': self.photo_url
+        }
