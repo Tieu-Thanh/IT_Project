56d8ee3630e142f0b0f4321f0bd52fa9a7f6dce8
Nguyen Pham Phu Thinh <105217439+Tieu-Thanh@users.noreply.github.com>
2023-12-28 20:02:44 +0700

    update README

--- backend/config.py
+++ backend/config.py
@@ -19,13 +19,13 @@ class Config:
         return firestore.client()
 
     # Firebase configuration
-    FIREBASE_API_KEY = env_config('FIREBASE_API_KEY', default='')
-    FIREBASE_AUTH_DOMAIN = env_config('FIREBASE_AUTH_DOMAIN', default='')
-    FIREBASE_PROJECT_ID = env_config('FIREBASE_PROJECT_ID', default='')
-    FIREBASE_STORAGE_BUCKET = env_config('FIREBASE_STORAGE_BUCKET', default='')
-    FIREBASE_MESSAGING_SENDER_ID = env_config('FIREBASE_MESSAGING_SENDER_ID', default='')
-    FIREBASE_APP_ID = env_config('FIREBASE_APP_ID', default='')
-    FIREBASE_MEASUREMENT_ID = env_config('FIREBASE_MEASUREMENT_ID', default='')
+    FIREBASE_API_KEY = env_config('FIREBASE_API_KEY', default='AIzaSyBLwxPmvV-fDQEBzwHm30WjaOOtOH7vaY8')
+    FIREBASE_AUTH_DOMAIN = env_config('FIREBASE_AUTH_DOMAIN', default='chuyen-de-nghien-cuu.firebaseapp.com')
+    FIREBASE_PROJECT_ID = env_config('FIREBASE_PROJECT_ID', default='chuyen-de-nghien-cuu')
+    FIREBASE_STORAGE_BUCKET = env_config('FIREBASE_STORAGE_BUCKET', default='chuyen-de-nghien-cuu.appspot.com')
+    FIREBASE_MESSAGING_SENDER_ID = env_config('FIREBASE_MESSAGING_SENDER_ID', default='220669649977')
+    FIREBASE_APP_ID = env_config('FIREBASE_APP_ID', default='1:220669649977:web:38ad854f120cc6b306d9e8')
+    FIREBASE_MEASUREMENT_ID = env_config('FIREBASE_MEASUREMENT_ID', default='G-4ZYD9VVTQS')
 
 
 class DevelopmentConfig(Config):
