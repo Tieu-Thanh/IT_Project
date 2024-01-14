6c422de473779107028519e76c2a8a14bcb52ae0
sceddd <socdia07@gmail.com>
2024-01-14 17:03:40 +0700

    annotate and delete and clear boundingbox

--- UI/app/src/main/java/finalproject/me/myapplication/AnnotateActivity.kt
+++ UI/app/src/main/java/finalproject/me/myapplication/AnnotateActivity.kt
@@ -2,19 +2,12 @@ package finalproject.me.myapplication
 
 import android.annotation.SuppressLint
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.gestures.awaitEachGesture
 import androidx.compose.foundation.gestures.awaitFirstDown
-import androidx.compose.foundation.gestures.calculatePan
-import androidx.compose.foundation.gestures.calculateRotation
-import androidx.compose.foundation.gestures.calculateZoom
-import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
@@ -39,25 +32,17 @@ import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.drawscope.DrawScope
 import androidx.compose.ui.graphics.drawscope.Stroke
-import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.input.pointer.PointerEventType
 import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.layout.onGloballyPositioned
-import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import finalproject.me.myapplication.ui.theme.MyApplicationTheme
-import kotlin.math.absoluteValue
 
 
 class AnnotateActivity : ComponentActivity() {
     private val TAG = "AnnotateActivity"
     private val imageResource = R.drawable.people
-    private var isZoomable = true
-    private val maxScale = 1f
-    private val minScale = 3f
-    private val isRotation = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -75,62 +60,32 @@ class AnnotateActivity : ComponentActivity() {
     @SuppressLint("MutableCollectionMutableState", "SuspiciousIndentation",
         "UnrememberedMutableState"
     )
-    @OptIn(ExperimentalFoundationApi::class)
     @Preview
     @Composable
     fun Annotate() {
         // Replace R.drawable.your_image_resource with the actual resource ID of your image
-        val rotationState = remember { mutableStateOf(1f) }
-        val scale = remember { mutableStateOf(1f) }
-        val offsetX = remember { mutableStateOf(1f) }
-        val offsetY = remember { mutableStateOf(1f) }
         val rectangle = remember {
             mutableStateOf(Rectangle(Offset(0f, 0f), Offset(0f, 0f)))
         }
-        var point1 = Offset(0f, 0f)
-        var point2: Offset
         var click = false
-        var imageWidth = 0.dp
-        var imageHeight = 0.dp
-        val rectangleList = remember{
+        var point1 = Offset(0f, 0f)
+        var point2:Offset
+        val rectangleList = remember {
             mutableStateListOf<Rectangle>()
         }
-        val density = LocalDensity.current
+        val clearRect = remember {
+            mutableStateOf(false)
+        }
         Box(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(16.dp)
-
-                .combinedClickable(
-                    interactionSource = remember { MutableInteractionSource() },
-                    indication = null,
-                    onClick = { /* NADA :) */ },
-                    onDoubleClick = {
-                        if (scale.value >= 2f) {
-                            scale.value = 1f
-                            offsetX.value = 1f
-                            offsetY.value = 1f
-                        } else scale.value = 3f
-                    },
-                )
                 .pointerInput(Unit) {
                     awaitEachGesture {
                         awaitFirstDown()
                         do {
                             val event = awaitPointerEvent()
-                            if (isZoomable) {
-                                scale.value *= event.calculateZoom()
-                                if (scale.value > 1) {
-                                    val offset = event.calculatePan()
-                                    offsetX.value += offset.x
-                                    offsetY.value += offset.y
-                                    rotationState.value += event.calculateRotation()
-                                } else {
-                                    scale.value = 1f
-                                    offsetX.value = 1f
-                                    offsetY.value = 1f
-                                }
-                            } else {
+                            if (!clearRect.value) {
                                 if (!click) {
                                     point1 = event.changes.first().position
                                     click = true
@@ -140,6 +95,24 @@ class AnnotateActivity : ComponentActivity() {
                                     if (event.type == PointerEventType.Release) {
                                         rectangleList.add(rectangle.value)
                                         click = false
+                                    }
+                                    else {
+                                        rectangleList.add(rectangle.value)
+                                        rectangleList.removeAt(rectangleList.size-1)
+                                    }
+                                }
+                            } else {
+                                if (event.type == PointerEventType.Release) {
+                                    rectangleList
+                                        .toList()
+                                        .filter {
+                                            it.topLeft.x < event.changes.first().position.x && it.topLeft.y < event.changes.first().position.y && it.bottomRight.x > event.changes.first().position.x && it.bottomRight.y > event.changes.first().position.y
+                                        }
+                                        .forEach {
+                                            rectangleList.remove(it)
+                                        }
+                                    if (rectangle.value.topLeft.x < event.changes.first().position.x && rectangle.value.topLeft.y < event.changes.first().position.y && rectangle.value.bottomRight.x > event.changes.first().position.x && rectangle.value.bottomRight.y > event.changes.first().position.y) {
+                                        rectangle.value = Rectangle(Offset(0f, 0f), Offset(0f, 0f))
                                     }
                                 }
                             }
@@ -156,57 +129,47 @@ class AnnotateActivity : ComponentActivity() {
                     .fillMaxHeight(0.5f) // Displaying the image on half of the screen height
                     .clip(shape = MaterialTheme.shapes.medium) // Optional: Clip the image with a rounded shape
                     .align(Alignment.Center)
-                    .onGloballyPositioned { coordinates ->
-                        imageWidth = with(density) {
-                            coordinates.size.width.toDp()
-                        }
-                        imageHeight = with(density) {
-                            coordinates.size.height.toDp()
-                        }
-                    }
-                    .graphicsLayer {
-                        if (isZoomable) {
-                            scaleX = maxOf(maxScale, minOf(minScale, scale.value))
-                            scaleY = maxOf(maxScale, minOf(minScale, scale.value))
-                            if (isRotation) {
-                                rotationZ = rotationState.value
-                            }
-                            if (offsetX.value > 0) {
-                                offsetX.value = minOf(offsetX.value, scaleX * imageWidth.value)
-                            } else {
-                                offsetX.value = maxOf(offsetX.value, -scaleX * imageWidth.value)
-                            }
-                            if (offsetY.value > 0) {
-                                offsetY.value = minOf(offsetY.value, scaleY * imageHeight.value)
-                            } else {
-                                offsetY.value = maxOf(offsetY.value, -scaleY * imageHeight.value)
-                            }
-                            translationX = offsetX.value
-                            translationY = offsetY.value
-                        }
-                    }
             )
             Button(
                 onClick = {
-                    isZoomable = !isZoomable
+                          clearRect.value = false
                 },
                 modifier = Modifier.align(Alignment.BottomCenter)
             ) {
                 Icon(imageVector = Icons.Default.AddCircle, contentDescription = null)
                 Text("Annotate")
             }
+            Button(
+                onClick = {
+                    rectangleList.clear()
+                    rectangle.value = Rectangle(Offset(0f, 0f), Offset(0f, 0f))
+                },
+                modifier = Modifier.align(Alignment.BottomStart)
+            ) {
+                Icon(imageVector = Icons.Default.AddCircle, contentDescription = null)
+                Text("Clear")
+            }
+            Button(
+                onClick = {
+                    clearRect.value = true
+                },
+                modifier = Modifier.align(Alignment.BottomEnd)
+            ){
+                Icon(imageVector = Icons.Default.AddCircle, contentDescription = null)
+                Text("Del")
+            }
             Canvas(
                 modifier = Modifier
                     .fillMaxWidth()
                     .fillMaxHeight(0.5f)
             ) {
-                drawBoundingBoxes(
-                    color = Color.Red,
-                    rectangles = rectangleList)
                 drawBoundingBox(
                     color = Color.Red,
                     rectangle = rectangle.value
                 )
+                drawBoundingBoxes(
+                    color = Color.Red,
+                    rectangles = rectangleList)
             }
         }
     }
@@ -236,6 +199,7 @@ class AnnotateActivity : ComponentActivity() {
         }
     }
 }
+
 
 
 data class Rectangle(val topLeft: Offset, val bottomRight: Offset)
