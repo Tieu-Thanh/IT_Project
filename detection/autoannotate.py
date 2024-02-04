336f327a98d7c1b49c6fe44e2b8d33b28891879f
nguyenthekhoig7 <nguyenthekhoig7@gmail.com>
2024-02-04 22:24:54 +0700

    Update code for easier use

--- detection/autoannotate.py
+++ detection/autoannotate.py
@@ -1,13 +1,46 @@
 from autodistill_grounded_sam import GroundedSAM
 from autodistill.detection import CaptionOntology
 import argparse
-import json 
-
 
 '''
 Usage:          python auoto-annotate.py    --classes ['banana','orange']\
                                             --input_folder path/to/images
 '''
+
+class Labeler():
+    def __init__(self):
+        pass
+
+    def label_images(self, classes: list, input_folder: str, output_folder: str = None, extension: str = '.jpeg'):
+        '''
+        Do detection on images, with desired classes.
+
+        Args:
+            classes: object names to be detected
+            input_folder: path to raw images
+            output_folder: path to output dataset (if leave empty, it will be automatically created)
+            extension: extension of image types to be process, default: jpeg image only.
+        '''
+        # Check required values
+        if not classes:
+            raise ValueError("classes is required")
+        if not input_folder:
+            raise ValueError("input_folder is required")
+
+
+        description_label_map = dict(zip(classes, classes))
+
+        print("Loading SAM model...")
+        base_model = GroundedSAM(ontology=CaptionOntology(
+            description_label_map
+        ))
+
+        print("Start annotation...")
+        dataset = base_model.label(
+            input_folder = input_folder, 
+            output_folder = output_folder,
+            extension=extension)
+        return dataset
 
 parser = argparse.ArgumentParser(description='Auto-annotate images and create dataset.')
 parser.add_argument('--classes', type=str, default='', help="(Required) List of classes to label, e.g. ['banana','orange']. Note: no blank space in between")
@@ -15,41 +48,16 @@ parser.add_argument('--input_folder', type=str, default='', help='(Required) Pat
 parser.add_argument('--output_folder', type=str, default=None, help='Path to folder where annotated images(dataset) will be saved')
 parser.add_argument('--extension', type=str, default='.jpeg', help="Extension of images to be annotated, e.g. ['jpeg', 'png', 'jpg']")
 
-args = parser.parse_args()
-print(args)
-
-classes_str = args.classes
-classes = classes_str.strip()[1:-1].split(',')
-# print(classes)
-description_label_map = dict(zip(classes, classes))
-input_folder = args.input_folder
-output_folder = args.output_folder
-extension = args.extension
-
-def label_images(description_label_map: dict, input_folder: str, output_folder: str = None, extension: str = 'jpeg'):
-    # Check required values
-    if not description_label_map:
-        raise ValueError("description_label_map is required")
-    if not input_folder:
-        raise ValueError("input_folder is required")
-
-    print("Load SAM model...")
-    # Load SAM model that do the detection and annotation
-    base_model = GroundedSAM(ontology=CaptionOntology(
-        description_label_map
-    ))
-
-    # Annotation
-    print("Start annotation...")
-    dataset = base_model.label(
-        input_folder = input_folder, 
-        output_folder = output_folder,
-        extension=extension,
-        record_confidence=True)
-
-    print("Dataset created done.")
-    return dataset
-
-
 if __name__ == '__main__':
-    label_images(description_label_map, input_folder, output_folder, extension)
+
+    args = parser.parse_args()
+    print(args)
+
+    classes_str = args.classes
+    classes = classes_str.strip()[1:-1].split(',')
+    input_folder = args.input_folder
+    output_folder = args.output_folder
+    extension = args.extension
+
+    labeler = Labeler()
+    labeler.label_images(classes, input_folder, output_folder, extension)
--- detection/example/label.py
+++ detection/example/label.py
@@ -1,14 +1,23 @@
 '''
 Usage: python label.py
 '''
+
+
 import sys
 import os
 current = os.path.dirname(os.path.realpath(__file__))
 parent = os.path.dirname(current)
 sys.path.append(parent)
+from autoannotate import Labeler
 
-from autoannotate import label_images
 
+# ### Example data ######
 classes = ['banana', 'orange']
-description_label_map = dict(zip(classes, classes))
-label_images(description_label_map=description_label_map, input_folder='data_sample')
+input_folder='./data_sample'
+
+# ### Run code ##########
+labeler = Labeler()
+labeler.label_images(classes, input_folder)
+
+
+# dataset will be saved at './data_sample_labeled'
