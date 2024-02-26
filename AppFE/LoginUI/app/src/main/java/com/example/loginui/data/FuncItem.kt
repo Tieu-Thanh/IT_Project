913aac39f592522d5e955a2b7722cd505589b164
sceddd <socdia07@gmail.com>
2024-02-26 15:01:39 +0700

    delete loginUi

--- AppFE/LoginUI/app/src/main/java/com/example/loginui/data/FuncItem.kt
+++
@@ -1,9 +0,0 @@
-package com.example.loginui.data
-
-import androidx.annotation.DrawableRes
-
-data class FuncItem(
-    val title: String,
-    val description: String,
-
-)
--- AppFE/LoginUI/app/src/main/java/com/example/loginui/data/SignInRequest.kt
+++
@@ -1,9 +0,0 @@
-package com.example.loginui.data
-
-data class SignInRequest(
-    val email: String,
-    val password: String,
-    val returnSecureToken: Boolean = true,
-    val tenantId: String? = null,
-)
-
